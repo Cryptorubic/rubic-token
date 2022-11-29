82c1bcba60f3fc9947d600b9daa847ab7855e8a6
debych <zh.eliseev@yandex.ru>
2022-11-29 15:19:05 +0300

    turn into ERC20Capped

--- contracts/RubicToken.sol
+++ contracts/RubicToken.sol
@@ -4,15 +4,16 @@ pragma solidity ^0.8.4;
 
 import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ERC20Capped} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 
-contract RubicToken is ERC20Burnable, Ownable{
+contract RubicToken is ERC20Burnable, ERC20Capped, Ownable{
     using SafeERC20 for IERC20;
 
     event SweepTokens(address token, uint256 amount, address recipient);
 
-    constructor() ERC20("RUBIC TOKEN", "RBC") {}
+    constructor() ERC20("RUBIC TOKEN", "RBC") ERC20Capped(1_000_000_000 ether) {}
 
     function sendToken(
         address _token,
@@ -48,9 +49,11 @@ contract RubicToken is ERC20Burnable, Ownable{
         emit SweepTokens(_token, _amount, _recipient);
     }
 
-    // TODO: REMOVE
+    function _mint(address account, uint256 amount) internal override(ERC20, ERC20Capped) {
+        ERC20Capped._mint(account, amount);
+    }
 
-    function mint(address _to, uint256 _amount) external {
+    function mint(address _to, uint256 _amount) external onlyOwner {
         _mint(_to, _amount);
     }
 }
