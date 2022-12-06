d306e319b3a806707fcbf66f8e463784bba179d9
debych <zh.eliseev@yandex.ru>
2022-12-06 13:24:48 +0300

    delete sweep

--- contracts/RubicToken.sol
+++ contracts/RubicToken.sol
@@ -3,51 +3,11 @@
 pragma solidity ^0.8.4;
 
 import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ERC20Capped} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 
 contract RubicToken is ERC20Burnable, ERC20Capped, Ownable{
-    using SafeERC20 for IERC20;
-
-    event SweepTokens(address token, uint256 amount, address recipient);
-
-    constructor() ERC20("RUBIC GOVERNANCE TOKEN", "RBC-GOV") ERC20Capped(1_000_000_000 ether) {}
-
-    function sendToken(
-        address _token,
-        uint256 _amount,
-        address _receiver
-    ) internal virtual {
-        if (_token == address(0)) {
-            Address.sendValue(
-                payable(_receiver),
-                _amount
-            );
-        } else {
-            IERC20(_token).safeTransfer(
-                _receiver,
-                _amount
-            );
-        }
-    }
-
-    /**
-     * @dev A function to rescue stuck tokens from the contract
-     * @param _token The token to sweep
-     * @param _amount The amount of tokens
-     * @param _recipient The recipient
-     */
-    function sweepTokens(
-        address _token,
-        uint256 _amount,
-        address _recipient
-    ) external onlyOwner {
-        sendToken(_token, _amount, _recipient);
-
-        emit SweepTokens(_token, _amount, _recipient);
-    }
+    constructor() ERC20("RUBIC TOKEN", "RBC") ERC20Capped(1_000_000_000 ether) {}
 
     function _mint(address account, uint256 amount) internal override(ERC20, ERC20Capped) {
         ERC20Capped._mint(account, amount);
